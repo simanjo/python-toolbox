0bba1621ca1741d8b2f0a959aacd9fc9aff99414
Jonathan Simantzik <82642377+simanjo@users.noreply.github.com>
2022-10-14 18:02:26 +0200

    added function to split string in parts with maximal length

--- util.py
+++ util.py
@@ -1,6 +1,7 @@
 import hashlib
 import warnings
 import subprocess
+from typing import List, Optional
 
 
 def get_file_hash(fname, chunksize=4096, algo='blake2b'):
@@ -37,3 +38,36 @@ def get_git_revision_hash() -> str:
     # CC BY-SA 4.0 https://creativecommons.org/licenses/by-sa/4.0/
     cmd = ['git', 'rev-parse', 'HEAD']
     return subprocess.check_output(cmd).decode('ascii').strip()
+
+
+def split_string_to_size(
+    string: str,
+    max_len: int,
+    sep: Optional[str] = None,
+) -> List[str]:
+    if (size := len(string)) <= max_len:
+        return [string]
+    if sep is None:
+        mod = size // max_len
+        counts = mod if size % max_len == 0 else mod+1
+        return [
+            string[i*max_len:(i+1)*max_len] for i in range(counts)
+        ]
+    else:
+        parts = string.split(sep)
+        result = []
+        current = []
+        cum_size = 0
+        for part in parts:
+            cum_size += len(part)
+            if cum_size > max_len:
+                if current:
+                    result.append(sep.join(current))
+                current = [part]
+                cum_size = len(part) + 1
+            else:
+                current.append(part)
+                cum_size += 1
+        if current:
+            result.append(sep.join(current))
+        return result
